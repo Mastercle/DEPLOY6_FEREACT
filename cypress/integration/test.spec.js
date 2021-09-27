47080a6e2f918739d682018772d2f7bc72a3ec8f
Cleon <44094714+Mastercle@users.noreply.github.com>
2021-09-27 17:15:09 -0400

    Update test.spec.js

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,10 +1,12 @@
 describe('Heading', () => {
     it('has the right title', () => {
-        cy.visit('http://172.31.90.30:5000/example-1')
+        cy.visit('http://172.31.90.33:5000/example-1')
 
         cy.get('h1')
             .invoke('text')
             .should("equal", "My Awesome Web Application")
+        
+        cy.screenshot()
     });
 
 });
